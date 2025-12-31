0265f53b868cc2dc2f10ab9de1ad55cb4e6ec04d
Sabir777 <sabir.yanin2014@yandex.ru>
2026-01-01 02:04:50 +0300

    Save

--- init_json_diff.py
+++ init_json_diff.py
@@ -21,11 +21,59 @@
 
 import sys
 import json
-from itertools import count
+import os
+from pathlib import Path
+# from itertools import count
 
 
-def print_json(json_object):
-    numbers = count(1)
+# def print_json(json_object):
+    # numbers = count(1)
+    # if type(json_object) == dict:
+        # print(f'словарь-{next(numbers)}')
+        # for key, val in json_object.items():
+            # print(f'key == {key}')
+            # if type(val) in (list, dict):
+                # print_json(val)
+            # else:
+                # print(f'"{val}"')
+    # else:
+        # print(f'список-{next(numbers)}')
+        # for val in json_object:
+            # if type(val) in (list, dict):
+                # print_json(val)
+            # else:
+                # print(f'"{val}"')
+
+
+
+def print_json(json_object, n_json, old_pwd):
+    """Функция для создания структуры из файлов и папок
+    в соответствии со структурой JSON-файла. Для каждого
+    вложенного объекта будет создаваться отдельная папка"""
+
+    # Каждый объект JSON - это итерируемый объект
+    # Если его элемент - это вложенный объект, то он обрабатывается рекурсивно
+
+    # Создаю файл JSON в текущей директории для данного объекта
+
+    # Определяю текущую директорию
+
+    for index, val in enumerate(json_object):
+        if type(val) in (list, dict):
+        match val:
+            case list():
+                old_pwd = Path.cwd()
+                new_dir = old_pwd / f'__{index}'
+                os.makedirs(new_dir, exist_ok=True) # Создаю папку для списка
+                os.chdir(new_dir) # Перехожу в новую папку
+                print_json(val, n_json, old_pwd)
+
+    if old_pwd != Path.cwd():
+        os.chdir('..') # Возвращаюсь в родительскую директорию
+
+
+
+
     if type(json_object) == dict:
         print(f'словарь-{next(numbers)}')
         for key, val in json_object.items():
@@ -42,22 +90,25 @@ def print_json(json_object):
             else:
                 print(f'"{val}"')
 
-# Получаю аргументы скрипта
-args = sys.argv[1:]
-if len(args) != 2:
-    sys.exit("В команду нужно передать два аргумента!!!\nАварийное завершение программы")
-
-one, two = args
-
-try:
-    # Открываю два сравниваемых файла JSON
-    with open(one) as first_file, open(two) as second_file:
-        # Получаю JSON в формате python
-        one = json.load(first_file)
-        two = json.load(second_file)
-except Exception as err:
-    sys.exit(f"Неверные типы переданных файлов:\n{err}")
 
 
-print_json(one)
+if __name__ == '__main__':
+    # Получаю аргументы скрипта
+    args = sys.argv[1:]
+    if len(args) != 2:
+        sys.exit("В команду нужно передать два аргумента!!!\nАварийное завершение программы")
+    one, two = args
+
+    try:
+        # Открываю два сравниваемых файла JSON
+        with open(one) as first_file, open(two) as second_file:
+            # Получаю JSON в формате python
+            one = json.load(first_file)
+            two = json.load(second_file)
+    except Exception as err:
+        sys.exit(f"Неверные типы переданных файлов:\n{err}")
+
+
+    _pwd = Path.cwd() # Текущая директория скрипта
+    print_json(one, 1, _pwd) # Передаю в функцию номер JSON и текущую директорию
 
