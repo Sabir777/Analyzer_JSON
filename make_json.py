5ab5272ea9aded09f38e2563974df622db02546b
Sabir777 <sabir.yanin2014@yandex.ru>
2026-01-06 17:40:21 +0300

    Сделал первую версию функции make_json - Нужно проверять

--- make_json.py
+++ make_json.py
@@ -28,8 +28,9 @@ import sys
 import json
 import os
 from pathlib import Path
-# from datetime import datetime
 import subprocess
+import copy
+# from datetime import datetime
 
 
 
@@ -103,90 +104,30 @@ def make_json(number, old_pwd):
         return file_json 
     elif change and file_json is None: # Изменения есть, но объект был удален
         return None 
-    elif not change: # Изменений нет - нужно рыть
-        # создавать родительские контейнеры только если где будет создан дочерний объект
-        # Изменить на нормальный код!!!!
-        # Перебор все папок до изменений: Метакод
-        for папка in dirs(pwd):
-            val = make_json(папка)
-            if val != None:
+    elif not change: # Если изменений нет в текущей папке, ищу их в дочерних папках
+
+        if file_json is None: # Создаю копию списка от другого json либо пустой словарь
+            file_json = copy.deepcopy(other_json) if type(other_json) == list else {}
+
+        for item in pwd.iterdir():
+            os.chdir(item) # Перехожу в дочернюю папку
+            value = make_json(number, pwd)
+            if value is not None:
                 change = True
-                obj[key] = val
+                # Удаляю префикс __ в названии папок для элементов списка
+                key = item.name.removeprefix("__")
+                file_json[key] = value
+
+        # После обработки вложенных объектов возвращаюсь в родительскую директорию (Произвольная вложенность)
+        # Ничего не делаю, если это стартовая директория скрипта
+        if old_pwd != pwd:
+            os.chdir('..')
+
+        # Если изменения были найдены возвращаю объект, в противном случае None
         if change:
-            return obj
+            return file_json
         else:
             return None
-        
-
-
-# def make_json(number, old_pwd):
-    # """Получаю новый python-объект после внесенных в проект изменений.
-    # После поиска изменений и возвращения результата он будет преобразован
-    # в JSON-объект и сохранен в новой папке"""
-
-    # # Текущая директория
-    # pwd = Path.cwd()
-
-
-    # # Проверяю существование файла
-    # file_dir = pwd / f'{number}.json'
-    # if file_dir.is_file():  # Если файл существует
-        # try:
-            # with open(file_dir) as file:
-                # # Получаю JSON в формате python
-                # obj_python = json.load(file)
-        # except Exception as err:
-            # sys.exit(f"Не удалось преобразовать {pwd}/{number}.json в python-объект")
-    # else: # Если файл не существует создаю пустой объект по типу существующего файла
-        # other = "2.json" if number == '1' else "1.json"
-        # try:
-            # with open(pwd / other) as file:
-                # # Получаю JSON в формате python
-                # obj_other = json.load(file)
-                # # Создаю пустой объект на основе другого файла
-                # obj_python = [] if type(obj_other) == list else {}
-        # except Exception as err:
-            # sys.exit(f"Не удалось преобразовать {pwd}/{other} в python-объект")
-
-
-    # # Если новый diff отличается от старого - возвращаю объект 
-    # if is_diff():
-        # return obj_python
-
-
-    # change = False  # изменения пока что не найдены
-    # if type(obj_python) == list:
-        # for i in range(len(obj_python)):
-            # try:
-                # nesting_dir = pwd / f'__{i}'
-                # if nesting_dir.is_dir():     # проверить существует ли вложенная папка
-                    # os.chdir(nesting_dir)    # перехожу во вложенную папку
-                    # result = make_json(number, pwd)
-                    # if result is not None:
-                        # change = True
-                        # obj_python[i] = result
-            # except Exception as e:
-                # sys.exit(f"Ошибка индекса для списка")
-    # else:   # если объект словарь
-        # for item in pwd.iterdir():
-            # if item.is_dir(): # Перебираю все папки
-                # os.chdir(nesting_dir) # перехожу во вложенную папку
-                # result = make_json(number, pwd)
-                # if result is not None:
-                    # change = True
-                    # obj_python[item.name] = result
-
-    # if change:
-        # return obj_python
-    # else:
-        # return None
-
-
-
-    # После обработки вложенных объектов возвращаюсь в родительскую директорию (Произвольная вложенность)
-    # Ничего не делаю, если это стартовая директория скрипта
-    if old_pwd != pwd:
-        os.chdir('..')
 
 
 
@@ -209,7 +150,6 @@ if __name__ == '__main__':
         sys.exit("Вы указали несуществующую папку для проекта!!!")
 
 
-
     # Перехожу в папку с проектом
     pwd = Path.cwd() / dir_project
     os.chdir(pwd)
@@ -220,7 +160,5 @@ if __name__ == '__main__':
     if obj_json is None:
         sys.exit("Проект не был изменен. Выгружать ничего не нужно!!!")
 
-    
-# функция возвращает словарь или список
-# - Получает путь (текщую директорию)
-# {генератор словаря} или [генератор списка]  -- наверху генератор-функция
+
+    print(obj_json)
